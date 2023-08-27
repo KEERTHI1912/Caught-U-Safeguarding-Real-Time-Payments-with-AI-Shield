79615859bb3b40b6d276c46a910ee4daabfbff10
KEERTHI1912 <sit20ad001@sairamtap.edu.in>
2023-08-27 14:28:43 +0530

    small change

--- FDS/settings.py
+++ FDS/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ['web-production-81cf.up.railway.app','127.0.0:1']
+ALLOWED_HOSTS = ['web-production-81cf.up.railway.app','127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-81cf.up.railway.app',]
 
